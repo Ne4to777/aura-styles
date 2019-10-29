b1c420a3789a914f479aa2d290247909f0091c20
Ne4to777 <ASAlekseev@dme.aero.corp>
2019-10-29 17:23:10 +0300

    refactoring

--- src/views/Classes.vue
+++ src/views/Classes.vue
@@ -1,5 +1,14 @@
 <template>
-	<div>
-		<h1>This is an about page</h1>
+	<div class="flex">
+		<div class="test">
+			<h1>This is an about page</h1>
+		</div>
+		<div class="test">
+			<h1>This is an about page</h1>
+		</div>
 	</div>
 </template>
+
+<style lang="stylus">
+@import './../assets/stylus/classes.styl'
+</style>
